a4de775d63fb87f5d04937bf782094a9bbdbc48b
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-06 03:27:32 +0000

    Add files via upload

---
+++ gellany_dga2_ml.py
@@ -0,0 +1,69 @@
+from sklearn import *
+import pandas as pd
+import numpy as np
+import pylab
+import tldextract
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+
+pylab.rcParams['figure.figsize'] = (14.0, 5.0)
+pylab.rcParams['axes.grid'] = True
+
+
+
+
+
+class dga():
+    
+    def __init__(self, uri =None):
+               
+               self.uri = uri
+    
+              
+
+    def domain_extract(self):
+              ext= tldextract.extract(self.uri)
+              if (not ext.suffix):
+                    return np.nan
+              else:
+                    return ext.domain
+
+
+    def print_domain_extract(self):
+            
+             
+            alexa_dataframe['domain'] = [dga(uri = uri).domain_extract() for uri in alexa_dataframe['uri']]
+            del alexa_dataframe['rank']
+            del alexa_dataframe['uri']
+            print(alexa_dataframe.head())
+
+    def preprocessing(self):
+            
+
+            # It's possible we have NaNs from blanklines or whatever
+            global alexa_dataframe2
+            alexa_dataframe2 = alexa_dataframe.fillna(0.0, inplace=True)
+            alexa_dataframe2 = alexa_dataframe.drop_duplicates()
+
+            # Set the class
+            alexa_dataframe2['class'] = 'legit'
+
+            # Shuffle the data (important for training/testing)
+            alexa_dataframe2 = alexa_dataframe2.reindex(np.random.permutation(alexa_dataframe2.index))
+            alexa_total = alexa_dataframe2.shape[0]
+            print('Total Alexa domains %d' % alexa_total)
+
+            # Hold out 10%
+            hold_out_alexa = alexa_dataframe2[alexa_total/10:]
+            alexa_dataframe2 = alexa_dataframe2[:alexa_total/10]
+
+            print('Number of Alexa domains: %d' % alexa_dataframe2.shape[0])
+
+
+            
+alexa_dataframe = pd.read_csv('data/alexa_100k.csv', names=['rank', 'uri'], header=None, encoding='utf-8')
+print(alexa_dataframe.head())
+print(alexa_dataframe.tail())            
+dga().print_domain_extract()
+dga().preprocessing()
+

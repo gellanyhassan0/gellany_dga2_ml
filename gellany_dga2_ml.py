f8d1a7a364c77cbc373c6186403854e1e940eed3
Elgilany Hassan <gellanyhassan0@gmail.com>
2022-06-06 06:08:47 +0000

    Add files via upload

--- gellany_dga2_ml.py
+++ gellany_dga2_ml.py
@@ -15,10 +15,10 @@ pylab.rcParams['axes.grid'] = True
 
 class dga():
     
-    def __init__(self, uri =None):
+    def __init__(self, uri =None, dataframe = None):
                
                self.uri = uri
-    
+               self.dataframe = dataframe
               
 
     def domain_extract(self):
@@ -32,38 +32,39 @@ class dga():
     def print_domain_extract(self):
             
              
-            alexa_dataframe['domain'] = [dga(uri = uri).domain_extract() for uri in alexa_dataframe['uri']]
+            self.dataframe['domain'] = [dga(uri = uri).domain_extract() for uri in self.dataframe['uri']]
             del alexa_dataframe['rank']
             del alexa_dataframe['uri']
-            print(alexa_dataframe.head())
+            print(self.dataframe.head())
 
     def preprocessing(self):
-            
-
             # It's possible we have NaNs from blanklines or whatever
-            global alexa_dataframe2
-            alexa_dataframe2 = alexa_dataframe.fillna(0.0, inplace=True)
-            alexa_dataframe2 = alexa_dataframe.drop_duplicates()
+            #global df
+            df = self.dataframe.fillna(0.0, inplace=True)
+            df = self.dataframe.drop_duplicates()
 
             # Set the class
-            alexa_dataframe2['class'] = 'legit'
+            df['class'] = 'legit'
 
             # Shuffle the data (important for training/testing)
-            alexa_dataframe2 = alexa_dataframe2.reindex(np.random.permutation(alexa_dataframe2.index))
-            alexa_total = len(alexa_dataframe2)
+            df = df.reindex(np.random.permutation(df.index))
+            alexa_total = len(df)
             print('Total Alexa domains %d' % alexa_total)
 
             # Hold out 10%
-            hold_out_alexa = alexa_dataframe2[:int(alexa_total*10/100)]
-            alexa_dataframe2 = alexa_dataframe2[:int(alexa_total*90/100)]
+            hold_out_alexa = df[:int(alexa_total*10/100)]
+            alexa_dataframe2 = df[:int(alexa_total*90/100)]
 
-            print('Number of Alexa domains: %d' % alexa_dataframe2.shape[0])
+            print('Number of Alexa domains: %d' % df.shape[0])
+            print(df.head())
 
-
-            
+        
 alexa_dataframe = pd.read_csv('data/alexa_100k.csv', names=['rank', 'uri'], header=None, encoding='utf-8')
+dga_dataframe = pd.read_csv('data/dga_domains.txt', names=['raw_domain'], header=None, encoding='utf-8')
+dga_dataframe['domain'] = dga_dataframe.applymap(lambda x: x.split('.')[0].strip().lower())
+
 print(alexa_dataframe.head())
 print(alexa_dataframe.tail())            
-dga().print_domain_extract()
-dga().preprocessing()
-
+dga(dataframe = alexa_dataframe).print_domain_extract()
+dga(dataframe = alexa_dataframe).preprocessing()
+dga(dataframe = dga_dataframe).preprocessing()
